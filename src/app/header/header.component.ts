0f41b0955664f4291499402a359b9d883159d4ef
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-04-24 16:52:47 -0300

    up

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,25 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   isMouseScroll:boolean = false;
+  isHeaderMobile: boolean = false;
 
   @HostListener('window:scroll',['$event']) onScroll() {
-    if(window.scrollY >= 60){
-      this.isMouseScroll = true;
+    if((window.scrollY >= 60) && (window.innerWidth>950)){
+      this.isMouseScroll = true;      
     } else {
       this.isMouseScroll = false;
     }
   }
+
+  onActiveMobileNav() {
+    window
+    this.isHeaderMobile = !this.isHeaderMobile;
+  }
+  
 }
