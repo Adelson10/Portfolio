01957421aa9a748056cdf16a55dffcd51c7d71c6
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-05-11 21:10:20 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
+import { SobreMimComponent } from './sobre-mim/sobre-mim.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ApresentacaoComponent,
+    SobreMimComponent,
   ],
   imports: [
     BrowserModule,
